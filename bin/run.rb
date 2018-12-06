6272a51cae8284a274d2961a31dbb7646b08ee26
github username <github email address>
2018-12-06 16:11:28 -0500

    okok

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,8 @@
 require_relative '../config/environment'
 a = Artii::Base.new :font => 'doom'
 puts a.asciify("Welcome to...")
+puts "🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨"
 puts a.asciify("Dream Doctor")
-binding.pry
+puts "🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨🌙✨"
 welcome
 
